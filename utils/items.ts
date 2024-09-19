45fa9f87e531ee48ec2badfa72cc03d9344183b6
h-sompura <sompharshi123@gmail.com>
2024-09-19 22:24:13 +0530

    chore(card-images): add front and back images of card and create an items util to store cards

---
+++ utils/items.ts
@@ -0,0 +1,39 @@
+const card_1_front = require("../assets/card-1-front.png");
+const card_2_front = require("../assets/card-2-front.png");
+const card_3_front = require("../assets/card-3-front.png");
+const card_4_front = require("../assets/card-4-front.png");
+const card_5_front = require("../assets/card-5-front.png");
+const card_6_front = require("../assets/card-6-front.png");
+const card_7_front = require("../assets/card-7-front.png");
+const card_8_front = require("../assets/card-8-front.png");
+
+const card_1_back = require("../assets/card-1-back.png");
+const card_2_back = require("../assets/card-2-back.png");
+const card_3_back = require("../assets/card-3-back.png");
+const card_4_back = require("../assets/card-4-back.png");
+const card_5_back = require("../assets/card-5-back.png");
+const card_6_back = require("../assets/card-6-back.png");
+const card_7_back = require("../assets/card-7-back.png");
+const card_8_back = require("../assets/card-8-back.png");
+
+export const cardFrontItems = [
+  card_1_front,
+  card_2_front,
+  card_3_front,
+  card_4_front,
+  card_5_front,
+  card_6_front,
+  card_7_front,
+  card_8_front,
+];
+
+export const cardBackItems = [
+  card_1_back,
+  card_2_back,
+  card_3_back,
+  card_4_back,
+  card_5_back,
+  card_6_back,
+  card_7_back,
+  card_8_back,
+];
